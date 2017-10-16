838f2ed2a676c0d15288a8684d88e67e5c13fb21
Adam Kirk <adamkirk@Adams-MacBook-Pro-2.local>
2017-10-16 11:17:47 +0200

    Add initial checkout with tags variable
    Add a checkoutWithTags script that will be exposed as a global var
    inside jenkins pipelines. This should allow the use of tags further
    down the pipelines.

---
+++ vars/checkoutWithTags.groovy
@@ -0,0 +1,10 @@
+def call() {
+    // Git-plugin 3.4.0 workaround for https://issues.jenkins-ci.org/browse/JENKINS-45164
+    checkout([
+        $class: 'GitSCM',
+        branches: scm.branches,
+        doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+        extensions: [[$class: 'CloneOption', noTags: false, shallow: false, depth: 0, reference: '']],
+        userRemoteConfigs: scm.userRemoteConfigs,
+    ])
+}
